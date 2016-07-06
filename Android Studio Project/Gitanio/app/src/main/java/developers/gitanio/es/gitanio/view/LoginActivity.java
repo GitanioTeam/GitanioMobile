fe610f7d5e5b84d94a0e0895ed8be1e9c5849e35
pauloXtr3m <paulo-gyn@outlook.com>
2016-07-05 21:41:31 -0300

    Removido o login com email

--- Android Studio Project/Gitanio/app/src/main/java/developers/gitanio/es/gitanio/view/LoginActivity.java
+++ Android Studio Project/Gitanio/app/src/main/java/developers/gitanio/es/gitanio/view/LoginActivity.java
@@ -99,10 +99,6 @@ public class LoginActivity extends AppCompatActivity {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
             cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
         }
 
         if (cancel) {
